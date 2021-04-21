f1ce0e4c57886b4db755ebb134e997fd66796bb2
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-04-21 20:10:56 +0200

    Add server 4

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ Check video about RRFTracker on https://www.youtube.com/watch?v=rVW8xczVpEo
 
 # Version
 
-version = '0.0.4'   # Version
+version = '0.0.5'   # Version
 env = 'prod'        # Environnement
 
 # Settings
